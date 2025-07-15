8599e3f65c37f8f950f4917144d2eda2626a7b27
marcelo_machado152 <marcelodossantosmachado05@gmail.com>
2025-07-15 17:20:27 -0300

    Atividades senai

---
+++ src/ATIVIDADES/JAVA/Dia_15_07_2025/ControleDeEstoqueDeProdutos.java
@@ -0,0 +1,9 @@
+package ATIVIDADES.JAVA.Dia_15_07_2025;
+
+import java.util.Scanner;
+
+public class Atividade15_07 {
+    public static void main(String[] args) {
+     
+}
+}
---
+++ src/ATIVIDADES/JAVA/Dia_15_07_2025/RegistroDeTemperaturasSemanais.java
@@ -0,0 +1,4 @@
+package ATIVIDADES.JAVA.Dia_15_07_2025;
+
+public class RegistroDeTemperaturasSemanais {
+}
